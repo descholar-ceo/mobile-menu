f3eacf856e0f4ef3fb2af313f9c7e7d57c0eea6a
descholar-ceo <emmanuellamugi@gmail.com>
2021-05-21 22:53:41 +0200

    Add js file for enabling toggle feature

---
+++ menu.js
@@ -0,0 +1,28 @@
+const menuToggle = document.querySelector('#menu-toggle');
+const mainElt = document.querySelectorAll('#main-content');
+const fullScreenMenu = document.querySelector('#full-screen-menu');
+const closeFullScreenMenuBtn = document.querySelector('#close-fullscreen-menu-btn');
+const fullMenuOption = document.querySelectorAll('#full-menu-option');
+
+menuToggle.addEventListener('click', () => {
+  fullScreenMenu.classList.remove('is-hidden');
+  mainElt.forEach(curr=>{
+    curr.classList.add('is-hidden');
+  });
+});
+
+closeFullScreenMenuBtn.addEventListener('click', event => {
+  fullScreenMenu.classList.add('is-hidden');
+  mainElt.forEach(curr=>{
+    curr.classList.remove('is-hidden');
+  });
+});
+
+fullMenuOption.forEach(curr=>{
+  curr.addEventListener('click', ()=>{
+    fullScreenMenu.classList.add('is-hidden');
+    mainElt.forEach(curr=>{
+      curr.classList.remove('is-hidden');
+    });
+  });
+});
